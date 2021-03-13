7fcf885fa4a83473039369b2b00a1af3ed82e9bb
maicol <maicoljosa910@gmail.com>
2021-03-12 21:52:34 -0500

    taller final maicol - wilson

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -28,5 +29,20 @@ export class HomePage {
     this.apellido = "";
     this.celular = "";
     this.email = "";
+  }
+
+  edit(usuario){
+    this.nombre= usuario.nombre;
+    this.apellido= usuario.apellido;
+    this.celular= usuario.celular;
+    this.email= usuario.email;
+
+    this.delete(usuario)
+    //console.log(usuarios)
+  }
+  
+  delete(usuario){
+    var dato = this.usuarios.indexOf(usuario)
+    this.usuarios.splice(dato,1);
   }
 }
